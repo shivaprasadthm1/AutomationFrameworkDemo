40bb24e5445a3bf406fe720240ad9fe6a28eaf9a
thm <thm@QWF5PB52K3.asiapacific.hpqcorp.net>
2024-03-09 10:16:13 +0530

    First commit

---
+++ demoFramework/src/test/java/demoFrameworkpractice/sample.java
@@ -0,0 +1,13 @@
+package demoFrameworkpractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample {
+
+	@Test
+	public void toLearnGitHub() {
+		Reporter.log("To Practice GitHub", true);
+	}
+
+}
